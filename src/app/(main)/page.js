128f1042d55f2ca0d59b04f8ebea0a7cc3253686
zahide sarp <sarpzahide@gmail.com>
2024-03-25 20:59:33 +0300

    day05

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,15 @@
-import Link from "next/link";
+import Slider from "@/components/homepage/slider";
+
+export const metadata = {
+	title: "Home",
+	description: "You can get luxury electornic devices",
+};
 
 export default function Home() {
-  return (
-    <main>
-     Hello nextjs
-    </main>
-  );
+	return (
+		<>
+			<Slider />
+			
+		</>
+	);
 }
